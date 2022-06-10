0371764ceed1d3ea17ae03d1d98942110cc78faf
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2022-06-11 00:55:08 +0900

    Now we can search the video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "descending" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -63,4 +63,17 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
