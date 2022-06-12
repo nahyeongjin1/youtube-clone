dd43c35de51d08a283401ab61b087f2de87b3a55
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2022-06-12 13:55:42 +0900

    Form validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,21 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   console.log(req.body);
-  const { name, email, username, password, location } = req.body;
+  const { name, email, username, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
   await User.create({
     name,
     email,
@@ -18,5 +32,3 @@ export const deleteUser = (req, res) => res.send("Delete User");
 export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See User Profile");
-
-/* Add Sign up page and hash password using bcrypt */
