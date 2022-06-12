d06dabe89960a139025d04ec6bce6d5ac4f194a7
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2022-06-12 13:13:30 +0900

    Add Sign Up page and hash password using bcrypt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,22 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  console.log(req.body);
+  const { name, email, username, password, location } = req.body;
+  await User.create({
+    name,
+    email,
+    username,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const deleteUser = (req, res) => res.send("Delete User");
 export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See User Profile");
+
+/* Add Sign up page and hash password using bcrypt */
